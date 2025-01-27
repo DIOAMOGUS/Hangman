fe646fe78808645d85b356cfe82a21c9a30bd8aa
DIO <122312027+DIOAMOGUS@users.noreply.github.com>
2025-01-27 19:16:18 +0000

    Hangman v1.1
    random quirks i changed and stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <iostream>
 #include <string>
 #include <string_view>
@@ -11,17 +10,7 @@
 // Add different languages
 // Add different difficulties (harder difficulties = longer words + less lives)
 // Add more words
-
-namespace WordList
-{
-    // Add as many words as you want! The function in this namespace allows it automatically
-    const std::vector<std::string_view> words { "absurd", "lucky", "subway", "pandemonium", "flawless", "jazz", "mystify", "electric", "backpack", "zephyr", "ember", "cinder", "megabyte", "mystery", "broccoli" , "account", "balls", "spaghetti", "opinion", "beautiful", "distance", "luggage" };
-
-    std::string_view getRandomWord()
-    {
-        return words[Random::get<std::size_t>(0, words.size()-1)];
-    }
-}
+// Temas
 
 #ifdef DEBUG
 void* operator new(size_t size)
@@ -37,19 +26,36 @@ void operator delete(void* ptr, size_t size)
 }
 #endif
 
+namespace Wordlist
+{
+    // Add as many words as you want! The function in this namespace allows it automatically
+    const std::vector<std::string_view> words{ "absurd", "lucky", "subway", "pandemonium", "flawless", "jazz", "mystify", "electric", "backpack", "zephyr", "ember", "cinder", "megabyte", "mystery", "broccoli" , "account", "balls", "spaghetti", "opinion", "beautiful", "distance", "luggage" };
+    
+    /*
+    const std::array<std::string_view, 50> wordsEasy{ "absurd", "lucky", "subway", "ember", "jazz", "backpack", "ember", "balls", "opinion", "account" };
+    const std::array<std::string_view, 50> wordsNormal{ "subway", "flawless",  "electric", "zephyr", "cinder", "megabyte", "mystery", "broccoli", "spaghetti", "opinion", "beautiful", "distance", "luggage" };
+    const std::array<std::string_view, 50> wordsHard{ "pandemonium", "mystify", "zephyr", "megabyte", "spaghetti", "opinion", "beautiful", "distance" };
+    */
+
+    std::string_view getRandomWord()
+    {
+        return words[Random::get<std::size_t>(0, words.size() - 1)];
+    }
+}
+
 int main() 
 {
-    std::string_view originalRandomWord = WordList::getRandomWord(); // Get a random word from the list
+    const std::string_view originalRandomWord = Wordlist::getRandomWord(); // Get a random word from the vector
     std::string hiddenWord{}; // Word that will be modified during the game
     setup(originalRandomWord, hiddenWord);
 
     std::vector<char> listOfGuesses{}; // List of guesses storing the user's already used characters so they don't repeat
-    constexpr std::array<char, 5> vowels { 'a', 'e', 'i', 'o', 'u' };
-    int lives = 7;
+    // constexpr std::array<char, 5> vowels { 'a', 'e', 'i', 'o', 'u' }; // unused for now
+    short lives = 7;
     while (lives != 0) {
         turn(originalRandomWord, hiddenWord, listOfGuesses, lives);
 
         winCheck(hiddenWord);
     }
-    std::cout << "You lose.\n";
+    std::cout << "You lose.\nThe Word was: " << originalRandomWord << '\n';
 }
